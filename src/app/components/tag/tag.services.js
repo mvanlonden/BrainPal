b0d59fd1214c334c1f01e035782639191b0a348c
William Wnekowicz <william.wnekowicz@gmail.com>
2015-09-19 23:37:28 -0700

    Update TagService to new API

--- src/app/components/tag/tag.services.js
+++ src/app/components/tag/tag.services.js
@@ -1,7 +1,15 @@
 var moduleName='tag.services';
 
 const HTTP = new WeakMap();
-const CLOUDBRAIN = 'http://demo.apiserver.cloudbrain.rocks/api/v1.0';
+const USER_ID = 'brainpal_user';
+const DEVICE_ID = 'brainpal_demo';
+const DEVICE_TYPE = 'muse';
+const METRICS = [ 'alpha_absolute',
+                  'beta_absolute',
+                  'gamma_absolute',
+                  'delta_absolute',
+                  'theta_absolute'];
+const CLOUDBRAIN = 'http://demo.apiserver.cloudbrain.rocks/api/v1.0/users/' + USER_ID;
 
 class TagService
 {
@@ -19,27 +27,11 @@ class TagService
       });
   }
 
-  getTag(tagName){
-    return HTTP.get(this).get(CLOUDBRAIN + `/tags/${tagName}`)
+  getTag(tagId){
+    return HTTP.get(this).get(CLOUDBRAIN + `/tags/${tagId}`)
       .then(result => result.data )
       .catch((error) => {
         this.$log.error('XHR Failed for getTag.\n' + angular.toJson(error.data, true));
-      });
-  }
-
-  getTagAggregates(tagId, type, metric, start_time, end_time){
-    type = type || 'sum';
-    
-    let params = {
-      aggregate_type: type,
-      metric: metric,
-      start: start_time,
-      end: end_time
-    };
-    return HTTP.get(this).get(CLOUDBRAIN + `/tags/${tagId}/aggregates`, { params: params })
-      .then(result => result.data )
-      .catch((error) => {
-        this.$log.error('XHR Failed for getTagAggregates.\n' + angular.toJson(error.data, true));
       });
   }
 
@@ -51,11 +43,30 @@ class TagService
       });
   }
 
+  getTagAggregates(tagId){
+    let filter_params = {
+      device_id: DEVICE_ID,
+      device_type: DEVICE_TYPE,
+      metric: METRICS
+    };
+    return HTTP.get(this).get(CLOUDBRAIN + `/tags/${tagId}/aggregates`, { filter_params })
+      .then(result => result.data )
+      .catch((error) => {
+        this.$log.error('XHR Failed for getTagAggregates.\n' + angular.toJson(error.data, true));
+      });
+  }
+
+  getTagAggregate(tagId, aggregateId){
+    return HTTP.get(this).get(CLOUDBRAIN + `/tags/${tagId}/aggregates/${aggregateId}`)
+      .then(result => result.data )
+      .catch((error) => {
+        this.$log.error('XHR Failed for getTagAggregate.\n' + angular.toJson(error.data, true));
+      });
+  }
+
   static tagFactory($http){
     return new TagService($http);
   }
 }
-
-TagService.tagFactory.$inject = ['$http'];
 
 export default TagService;
